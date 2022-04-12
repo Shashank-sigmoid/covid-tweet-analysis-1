8c0099edce3175295f51834a141c62b5ea988ab5
shantanu-sigmoid <shantanut@sigmoidanalytics.com>
2022-04-12 09:26:35 +0530

    added get request based on pipeline

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,5 +1,4 @@
-// https://riptutorial.com/akka/example/31018/akka-http-server--hello-world--scala-dsl-
-import SparkMongo.fetchAllJsonString
+import SparkMongo.{fetchAllJsonString, fetchQueryJsonString}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
@@ -12,23 +11,36 @@ object Server extends App {
 
   implicit val system: ActorSystem = ActorSystem("ProxySystem")
 
+  // Pipeline for mongo query
+  val pipeline: String = "[{ $match: { user_location: { $exists: true } } }]"
+
+  // Route setup for localhost
   val route = pathPrefix("api"){
     concat(
+      // Welcome Page
       get{
         path("hello"){
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
         }
       },
+      // Fetch all content from database
       get{
         path("all"){
           complete(HttpEntity(ContentTypes.`application/json`, fetchAllJsonString()))
+        }
+      },
+      // Fetch content based on pipeline
+      get{
+        path("query"){
+          complete(HttpEntity(ContentTypes.`application/json`, fetchQueryJsonString(pipeline)))
         }
       }
     )
 
   }
-
+  // Bind route at localhost:8080
   val bindingFuture = Http().newServerAt("127.0.0.1", port = 8080).bindFlow(route)
+  // Wait for infinite second before terminating
   Await.result(system.whenTerminated, Duration.Inf)
 
 }
