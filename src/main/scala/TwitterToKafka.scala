752da9a28b86da4d71dab21aee80e3f881e170fd
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-04-25 23:56:48 +0530

    Fetching tweets using twitter4j

--- src/main/scala/TwitterToKafka.scala
+++ src/main/scala/TwitterToKafka.scala
@@ -1,58 +1,72 @@
-import com.google.common.collect.Lists
-import com.twitter.hbc.ClientBuilder
-import com.twitter.hbc.core.Constants
-import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint
-import com.twitter.hbc.core.processor.StringDelimitedProcessor
-import com.twitter.hbc.httpclient.auth.OAuth1
-import com.typesafe.config.ConfigFactory
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
-
+import com.typesafe.config.ConfigFactory
+import twitter4j.{FilterQuery, StallWarning, Status, StatusDeletionNotice, StatusListener, TwitterStreamFactory, TwitterObjectFactory}
+import twitter4j.conf.ConfigurationBuilder
 import scala.io.Source
 import java.util.Properties
 import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
 
-
-object TwitterToKafka {
+object TwitterToKafka{
 
   @throws[InterruptedException]
   def run(apiKey: String, apiKeySecret: String, accessToken: String, accessTokenSecret: String): Unit = {
 
+    // Reading keywords from txt file
+    val filename = "src/main/resources/keywords.txt"
+    var keywords: String = ""
+    for(line <- Source.fromFile(filename).getLines) {
+      keywords += line + ", "
+    }
+
     // Create an appropriately sized blocking queue
     val queue = new LinkedBlockingQueue[String](10000)
 
-    // Endpoint use to track terms
-    val endpoint = new StatusesFilterEndpoint
+    val cb = new ConfigurationBuilder()
+      .setDebugEnabled(true)
+      .setJSONStoreEnabled(true)
+      .setOAuthConsumerKey(apiKey)
+      .setOAuthConsumerSecret(apiKeySecret)
+      .setOAuthAccessToken(accessToken)
+      .setOAuthAccessTokenSecret(accessTokenSecret)
+      .setTweetModeExtended(true)
 
-    // Add some track terms
-    // reading keywords from text file
-    val filename = "src/main/resources/keywords.txt"
-    var keywords= ""
-    for (keyword <- Source.fromFile(filename).getLines) {
-      keywords += keyword.toLowerCase + ", "
+    val twitterStream = new TwitterStreamFactory(cb.build()).getInstance()
+    val listener = new StatusListener() {
+      def onStatus(status: Status) {
+        val result = TwitterObjectFactory.getRawJSON(status)
+        queue.offer(result)
+      }
+      def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = {
+        println("Got a status deletion notice ID: " + statusDeletionNotice.getStatusId)
+      }
+
+      def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = {
+        println("Got track limitation notice: " + numberOfLimitedStatuses)
+      }
+
+      def onScrubGeo(userId: Long, upToStatusId: Long): Unit = {
+        println("Got scrub_geo event userId: " + userId + "upToStatusId: " + upToStatusId)
+      }
+
+      def onStallWarning(warning: StallWarning): Unit = {
+        println("Got stall warning: " + warning)
+      }
+
+      def onException(ex: Exception) {
+        ex.printStackTrace
+      }
     }
-
-    endpoint.trackTerms(Lists.newArrayList(keywords))
-
-    // Define auth structure
-    val auth = new OAuth1(apiKey, apiKeySecret, accessToken, accessTokenSecret)
-
-    // Create a new BasicClient. By default gzip is enabled.
-    val client = new ClientBuilder()
-      .name("sampleExampleClient")
-      .hosts(Constants.STREAM_HOST)
-      .endpoint(endpoint)
-      .authentication(auth)
-      .processor(new StringDelimitedProcessor(queue))
-      .build
-
-    // Connect the client with the above configuration
-    client.connect()
+    twitterStream.addListener(listener)
+    val query = new FilterQuery().track(keywords)
+    twitterStream.filter(query)
 
     // Set configuration for kafka
     val kafkaProducerProps: Properties = {
       val props = new Properties()
       props.put("bootstrap.servers", "localhost:9092")
+      props.put("acks", "all")
+      props.put("retries", 0)
       props.put("key.serializer", classOf[StringSerializer].getName)
       props.put("value.serializer", classOf[StringSerializer].getName)
       props
@@ -61,35 +75,35 @@ object TwitterToKafka {
     // Create a producer for kafka
     val producer = new KafkaProducer[String, String](kafkaProducerProps)
 
-    // Take 3 messages from stream and push it to kafka topic named test-topic with null key
-    for (msgRead <- 0 until 3) {
+    // Take x messages from stream and push it to kafka topic named covid-tweet
+    var r = 0
+    for (_ <- 0 until 1000) {
       val msg =  queue.poll(5, TimeUnit.SECONDS)
-      print(msg)
       if (msg != null) {
-        producer.send(new ProducerRecord[String, String]("test-topic", null, msg))
+        // println(msg)
+        r += 1
+        producer.send(new ProducerRecord[String, String]("covid-tweet", null, msg))
       }
     }
-
-    // Stop the client
-    client.stop()
-
-    // Print no. of tweets fetched
-    println(s"The client read ${client.getStatsTracker.getNumMessages - 1} messages!")
+    print(s"No. of messages fetched: ${r}")
+    producer.close()
+    twitterStream.cleanUp()
+    twitterStream.shutdown()
   }
 
   def main(args: Array[String]): Unit = {
-
     val config = ConfigFactory.load()
-    val apiKey = config.getString("apiKey")
-    val apiKeySecret = config.getString("apiKeySecret")
+    val consumerKey = config.getString("apiKey")
+    val consumerSecret = config.getString("apiKeySecret")
     val accessToken = config.getString("accessToken")
     val accessTokenSecret = config.getString("accessTokenSecret")
 
-    System.setProperty("apiKey", apiKey)
-    System.setProperty("apiKeySecret", apiKeySecret)
+    System.setProperty("apiKey", consumerKey)
+    System.setProperty("apiKeySecret", consumerSecret)
     System.setProperty("accessToken", accessToken)
     System.setProperty("accessTokenSecret", accessTokenSecret)
-    try TwitterToKafka.run(apiKey, apiKeySecret, accessToken, accessTokenSecret)
+
+    try TwitterToKafka.run(consumerKey, consumerSecret, accessToken, accessTokenSecret)
     catch {
       case e: InterruptedException =>
         System.out.println(e)
