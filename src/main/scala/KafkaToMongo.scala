d0437878ae86a96569792be64fe7d7dda73aa97d
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-04-26 00:00:31 +0530

    Topic name changed

--- src/main/scala/KafkaToMongo.scala
+++ src/main/scala/KafkaToMongo.scala
@@ -41,7 +41,7 @@ object KafkaToMongo {
     val df = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "test-topic")
+      .option("subscribe", "covid-tweet")
       .option("startingOffsets", "earliest")
       .load()
     //     df
