a272f3fddc40a28f5cc0efc765b854f26fc99290
shantanu-sigmoid <shantanut@sigmoidanalytics.com>
2022-05-10 11:30:06 +0530

    refactored code, files and added comments

--- src/main/scala/TwitterToKafka.scala
+++ src/main/scala/TwitterToKafka.scala
@@ -7,44 +7,57 @@ import scala.io.Source
 import java.util.Properties
 import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
 
+/*
+    Takes input as authentication strings for TwitterAPI
+    and push the streaming tweets (filtered) into Kafka Topic
+*/
 object TwitterToKafka{
 
   @throws[InterruptedException]
   def run(apiKey: String, apiKeySecret: String, accessToken: String, accessTokenSecret: String): Unit = {
 
-    // Reading keywords from txt file
+    // Reading keywords related to coronavirus from txt file
     val filename = "src/main/resources/keywords.txt"
+    // `keywords` will contain appropriate format from keywords to pass while tracking and selecting specific tweets
     var keywords: String = ""
     for(line <- Source.fromFile(filename).getLines) {
       keywords += line + ", "
     }
 
-    // Create an appropriately sized blocking queue
+    // Create an appropriately sized blocking custom queue
     val queue = new LinkedBlockingQueue[String](10000)
 
     val cb = new ConfigurationBuilder()
-      .setDebugEnabled(true)
-      .setJSONStoreEnabled(true)
+      .setDebugEnabled(true)              // Enables deubg output
+      .setJSONStoreEnabled(true)          // raw JSON forms will be stored in DataObjectFactory.
       .setOAuthConsumerKey(apiKey)
       .setOAuthConsumerSecret(apiKeySecret)
       .setOAuthAccessToken(accessToken)
       .setOAuthAccessTokenSecret(accessTokenSecret)
-      .setTweetModeExtended(true)
+      .setTweetModeExtended(true) 
 
+    // Creates a TwitterStreamFactory with the predefined `cb` configuration.
     val twitterStream = new TwitterStreamFactory(cb.build()).getInstance()
+
+    // Create a Listener for the Stream 
     val listener = new StatusListener() {
+
+      // For every status change, add result in the queue (if not full)
       def onStatus(status: Status) {
         val result = TwitterObjectFactory.getRawJSON(status)
         queue.offer(result)
       }
+
+      // This notice will be sent each time a limited stream becomes unlimited
       def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = {
         println("Got a status deletion notice ID: " + statusDeletionNotice.getStatusId)
       }
 
+      // Called upon location deletion messages.
       def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = {
         println("Got track limitation notice: " + numberOfLimitedStatuses)
       }
-
+      // Called when receiving stall warnings.
       def onScrubGeo(userId: Long, upToStatusId: Long): Unit = {
         println("Got scrub_geo event userId: " + userId + "upToStatusId: " + upToStatusId)
       }
@@ -57,8 +70,12 @@ object TwitterToKafka{
         ex.printStackTrace
       }
     }
+
+    // Add listener to the Stream
     twitterStream.addListener(listener)
+    // Create a FilterQuery based on keywords (related to coronavirus)
     val query = new FilterQuery().track(keywords)
+    // Apply the filter to the stream
     twitterStream.filter(query)
 
     // Set configuration for kafka
@@ -66,7 +83,6 @@ object TwitterToKafka{
       val props = new Properties()
       props.put("bootstrap.servers", "localhost:9092")
       props.put("acks", "all")
-//      props.put("retries", 0)
       props.put("key.serializer", classOf[StringSerializer].getName)
       props.put("value.serializer", classOf[StringSerializer].getName)
       props
@@ -75,39 +91,45 @@ object TwitterToKafka{
     // Create a producer for kafka
     val producer = new KafkaProducer[String, String](kafkaProducerProps)
 
-    // Take x messages from stream and push it to kafka topic named covid-tweet
+    // Take x messages from stream and push it to kafka topic named `covid-tweet`
     var r = 0
     for (_ <- 0 until 1000) {
+      // Poll the message from the queue with timeout of 5 seconds
       val msg =  queue.poll(5, TimeUnit.SECONDS)
       if (msg != null) {
         // println(msg)
         r += 1
+        // Create a new producer record and send to kafka with topic: "covid-tweet", key: "null", message: "message"
         producer.send(new ProducerRecord[String, String]("covid-tweet", null, msg))
       }
     }
+
     print(s"No. of messages fetched: ${r}")
+    // Close the producer connected to Kafka Topic
     producer.close()
+    // Clean and Shutdown internal stream consuming thread
     twitterStream.cleanUp()
     twitterStream.shutdown()
   }
 
   def main(args: Array[String]): Unit = {
+    // By default, the ConfigFactory looks for a configuration file called application.properties
     val config = ConfigFactory.load()
-//    val consumerKey = config.getString("apiKey")
-//    val consumerSecret = config.getString("apiKeySecret")
-//    val accessToken = config.getString("accessToken")
-//    val accessTokenSecret = config.getString("accessTokenSecret")
-    val accessToken: String="1509882730707775488-7Aa0roBpZ18YRHlwFAIQxwqvymWohb"
-    val accessTokenSecret: String="E8oXSHCg9uXGIgxi5RvvfBRiVc95FsMjhfvmZhNNtR0u8"
-    val consumerKey: String="4ep0QIoguktMBmWgvWHpBcKon"
-    val consumerSecret: String="qO250zSrdGHmAdpKgM3s7PYCpTH835dzF6R8cKfYyWWgVC0bwZ"
 
-    System.setProperty("apiKey", consumerKey)
-    System.setProperty("apiKeySecret", consumerSecret)
+    // Load the secrets from application.properties
+    val apiKey = config.getString("apiKey")
+    val apiKeySecret = config.getString("apiKeySecret")
+    val accessToken = config.getString("accessToken")
+    val accessTokenSecret = config.getString("accessTokenSecret")
+
+    // Set the property in the running JVM instance
+    System.setProperty("apiKey", apiKey)
+    System.setProperty("apiKeySecret", apiKeySecret)
     System.setProperty("accessToken", accessToken)
     System.setProperty("accessTokenSecret", accessTokenSecret)
 
-    try TwitterToKafka.run(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+    // Start transferring the tweets from Twitter API to Kafka Topic
+    try TwitterToKafka.run(apiKey, apiKeySecret, accessToken, accessTokenSecret)
     catch {
       case e: InterruptedException =>
         System.out.println(e)
